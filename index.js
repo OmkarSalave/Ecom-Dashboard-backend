9d7ea2bf097d16ee6dec5e3aa9c0d8d05df4d7a0
Omkar Salave for Ecom-Dashboard-backend <omkarsalave08@gmail.com>
2024-03-05 00:40:26 +0530

    solving cors"

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors("*"));
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*"); // Adjust the origin as needed
